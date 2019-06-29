06574fa0a2498097bf3efd052b7efade97cce7a0
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-06-30 06:40:33 +0700

    audio_conf: Introduce QCOM Power Gating
    Introduce QCOM Power Gating toggles
    
    User can active/disable qualcomm power gating features using M.A.O instead using terminal or directly disable from kernel, also improve cardview for unavailable features that will not show if not exist.
    
    Also introduce Experimental features as qcom power gating integration show up, experimental features will use sharedpreferences to save user configuration.
    
    Current Bug:
    - On first initial launch of app (after clear data or initial install) M.A.O can't detect correctly states for each kernel audio status. So it need to re-open activity of audio configuration after first open, and M.A.O will work correctly.
    
    I'll try to solve this issue later..

--- app/src/main/java/com/hana/mao/audio_conf.java
+++ app/src/main/java/com/hana/mao/audio_conf.java
@@ -1,8 +1,11 @@
 package com.hana.mao;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -17,7 +20,10 @@ import java.io.FileInputStream;
 
 public class audio_conf extends AppCompatActivity {
 
-    private Switch uhqa, hph, impedance, amp;
+    private CardView uhqa, hph, impedance, amp, exp, qcom_gating;
+    private Switch s_uhqa, s_hph, s_impedance, s_amp, s_exp, s_qcom_gating;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +32,39 @@ public class audio_conf extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_audio_conf);
 
-        uhqa = findViewById(R.id.uhqa);
-        hph = findViewById(R.id.hph);
-        amp = findViewById(R.id.amp);
-        impedance = findViewById(R.id.impedance);
+        //Define cardview layout
+        uhqa = findViewById(R.id.cv_uhqa);
+        hph = findViewById(R.id.cv_hph);
+        amp = findViewById(R.id.cv_amp);
+        impedance = findViewById(R.id.cv_impedance);
+        exp = findViewById(R.id.cv_experimental);
+        qcom_gating = findViewById(R.id.cv_qcom_gating);
+
+        //Define switch layout
+        s_uhqa = findViewById(R.id.uhqa);
+        s_hph = findViewById(R.id.hph);
+        s_amp = findViewById(R.id.amp);
+        s_impedance = findViewById(R.id.impedance);
+        s_exp = findViewById(R.id.exp);
+        s_qcom_gating = findViewById(R.id.qcom_gating);
+
+        preferences = getSharedPreferences("exp_pref", Context.MODE_PRIVATE);
 
         File uhqa_file = new File("/sys/module/snd_soc_wcd9335/parameters/huwifi_mode");
         File hph_file = new File("/sys/module/snd_soc_wcd9330/parameters/high_perf_mode");
         File amp_file = new File("/sys/module/snd_soc_wcd9335/parameters/low_distort_amp");
         File impedance_file = new File("/sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en");
+        File qcom_gating_file = new File("/sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable");
+        File exp_file = new File("/data/data/com.hana.mao/shared_prefs/exp_pref.xml");
 
         FileInputStream fstream;
         Clean();
+        exp_check();
         uhqa_dump();
         hph_dump();
         amp_dump();
         impedance_dump();
+        qcom_gating_dump();
 
         if(uhqa_file.exists()){
             uhqa.setVisibility(View.VISIBLE);
@@ -55,14 +78,14 @@ public class audio_conf extends AppCompatActivity {
                 fstream.close();
                 String details[] = sbuffer.toString().split("\n");
                 if (details[0].equals("1")){
-                    uhqa.setVisibility(View.VISIBLE);
-                    uhqa.setChecked(true);
-                    uhqa.setOnClickListener(new View.OnClickListener() {
+                    s_uhqa.setVisibility(View.VISIBLE);
+                    s_uhqa.setChecked(true);
+                    s_uhqa.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(uhqa.isChecked())
+                            if(s_uhqa.isChecked())
                             {
                                 try {
                                     CommandResult uhqa = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9335/parameters/huwifi_mode");
@@ -83,14 +106,14 @@ public class audio_conf extends AppCompatActivity {
                         }
                     });
                 } if (details[0].equals("0")){
-                    uhqa.setVisibility(View.VISIBLE);
-                    uhqa.setChecked(false);
-                    uhqa.setOnClickListener(new View.OnClickListener() {
+                    s_uhqa.setVisibility(View.VISIBLE);
+                    s_uhqa.setChecked(false);
+                    s_uhqa.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(uhqa.isChecked())
+                            if(s_uhqa.isChecked())
                             {
                                 try {
                                     CommandResult uhqa = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9335/parameters/huwifi_mode");
@@ -116,7 +139,7 @@ public class audio_conf extends AppCompatActivity {
             }
         } else {
             uhqa.setVisibility(View.GONE);
-            Toast.makeText(audio_conf.this, "UHQA Not Found", Toast.LENGTH_SHORT).show();
+            Toast.makeText(audio_conf.this, "Ultra High Quality Audio Not Found", Toast.LENGTH_LONG).show();
         }
 
         if(hph_file.exists()){
@@ -131,14 +154,14 @@ public class audio_conf extends AppCompatActivity {
                 fstream.close();
                 String details[] = sbuffer.toString().split("\n");
                 if (details[0].equals("1")){
-                    hph.setVisibility(View.VISIBLE);
-                    hph.setChecked(true);
-                    hph.setOnClickListener(new View.OnClickListener() {
+                    s_hph.setVisibility(View.VISIBLE);
+                    s_hph.setChecked(true);
+                    s_hph.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(hph.isChecked())
+                            if(s_hph.isChecked())
                             {
                                 try {
                                     CommandResult hph = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9330/parameters/high_perf_mode");
@@ -160,13 +183,13 @@ public class audio_conf extends AppCompatActivity {
                     });
                 } if (details[0].equals("0")){
                     hph.setVisibility(View.VISIBLE);
-                    hph.setChecked(false);
-                    hph.setOnClickListener(new View.OnClickListener() {
+                    s_hph.setChecked(false);
+                    s_hph.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(hph.isChecked())
+                            if(s_hph.isChecked())
                             {
                                 try {
                                     CommandResult hph = Shell.SU.run("echo \"1\" > //sys/module/snd_soc_wcd9330/parameters/high_perf_mode");
@@ -192,7 +215,7 @@ public class audio_conf extends AppCompatActivity {
             }
         } else {
             hph.setVisibility(View.GONE);
-            Toast.makeText(audio_conf.this, "Headset High Performance Mode Not Found", Toast.LENGTH_SHORT).show();
+            Toast.makeText(audio_conf.this, "Headset High Performance Mode Not Found", Toast.LENGTH_LONG).show();
         }
 
         if(amp_file.exists()){
@@ -208,13 +231,13 @@ public class audio_conf extends AppCompatActivity {
                 String details[] = sbuffer.toString().split("\n");
                 if (details[0].equals("1")){
                     amp.setVisibility(View.VISIBLE);
-                    amp.setChecked(true);
-                    amp.setOnClickListener(new View.OnClickListener() {
+                    s_amp.setChecked(true);
+                    s_amp.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(amp.isChecked())
+                            if(s_amp.isChecked())
                             {
                                 try {
                                     CommandResult amp = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9335/parameters/low_distort_amp");
@@ -236,13 +259,13 @@ public class audio_conf extends AppCompatActivity {
                     });
                 } if (details[0].equals("0")){
                     amp.setVisibility(View.VISIBLE);
-                    amp.setChecked(false);
-                    amp.setOnClickListener(new View.OnClickListener() {
+                    s_amp.setChecked(false);
+                    s_amp.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(amp.isChecked())
+                            if(s_amp.isChecked())
                             {
                                 try {
                                     CommandResult amp = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9335/parameters/low_distort_amp");
@@ -268,7 +291,7 @@ public class audio_conf extends AppCompatActivity {
             }
         } else {
             amp.setVisibility(View.GONE);
-            Toast.makeText(audio_conf.this, "Low Distortion AMP Not Found", Toast.LENGTH_SHORT).show();
+            Toast.makeText(audio_conf.this, "Low Distortion AMP Not Found", Toast.LENGTH_LONG).show();
         }
 
         if(impedance_file.exists()){
@@ -284,13 +307,13 @@ public class audio_conf extends AppCompatActivity {
                 String details[] = sbuffer.toString().split("\n");
                 if (details[0].equals("1")){
                     impedance.setVisibility(View.VISIBLE);
-                    impedance.setChecked(true);
-                    impedance.setOnClickListener(new View.OnClickListener() {
+                    s_impedance.setChecked(true);
+                    s_impedance.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(impedance.isChecked())
+                            if(s_impedance.isChecked())
                             {
                                 try {
                                     CommandResult impedance = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en");
@@ -312,13 +335,13 @@ public class audio_conf extends AppCompatActivity {
                     });
                 } if (details[0].equals("0")){
                     impedance.setVisibility(View.VISIBLE);
-                    impedance.setChecked(false);
-                    impedance.setOnClickListener(new View.OnClickListener() {
+                    s_impedance.setChecked(false);
+                    s_impedance.setOnClickListener(new View.OnClickListener() {
 
                         @Override
                         public void onClick(View v) {
 
-                            if(impedance.isChecked())
+                            if(s_impedance.isChecked())
                             {
                                 try {
                                     CommandResult impedance = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en");
@@ -343,8 +366,118 @@ public class audio_conf extends AppCompatActivity {
                 e.printStackTrace();
             }
         } else {
-            amp.setVisibility(View.GONE);
-            Toast.makeText(audio_conf.this, "Headphone Impedance Detection Not Found", Toast.LENGTH_SHORT).show();
+            impedance.setVisibility(View.GONE);
+            Toast.makeText(audio_conf.this, "Headphone Impedance Detection Not Found", Toast.LENGTH_LONG).show();
+        }
+
+        s_exp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            if(s_exp.isChecked())
+            {
+                try {
+                    qcom_gating.setVisibility(View.VISIBLE);
+                    String exp_pref = "1";
+                    editor = preferences.edit();
+                    editor.putString("EXP", exp_pref);
+                    editor.apply();
+                    Toast.makeText(audio_conf.this, "Experimental features is active", Toast.LENGTH_SHORT).show();
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }  else {
+                try {
+                    qcom_gating.setVisibility(View.GONE);
+                    String exp_pref = "0";
+                    editor = preferences.edit();
+                    editor.putString("EXP", exp_pref);
+                    editor.apply();
+                    Toast.makeText(audio_conf.this, "Experimental features is not active", Toast.LENGTH_SHORT).show();
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+        }}
+    });
+
+        if(qcom_gating_file.exists()){
+            try {
+                fstream = openFileInput("qcom_gating.txt");
+                StringBuffer sbuffer = new StringBuffer();
+                int i;
+                while ((i = fstream.read())!= -1){
+                    sbuffer.append((char)i);
+                }
+                fstream.close();
+                String details[] = sbuffer.toString().split("\n");
+                if (details[0].equals("1")){
+                    s_qcom_gating.setChecked(true);
+                    s_qcom_gating.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+
+                            if(s_qcom_gating.isChecked())
+                            {
+                                try {
+                                    CommandResult qcom_gating = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable");
+                                    Toast.makeText(audio_conf.this, "QCOM Gating is Active", Toast.LENGTH_SHORT).show();
+                                }
+                                catch(Exception e){
+                                    e.printStackTrace();
+                                }
+                            }  else {
+                                try {
+                                    CommandResult qcom_gating = Shell.SU.run("echo \"0\" > /sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable");
+                                    Toast.makeText(audio_conf.this, "QCOM Gating is Disabled", Toast.LENGTH_SHORT).show();
+                                }
+                                catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+                } if (details[0].equals("0")){
+                    s_qcom_gating.setChecked(false);
+                    s_qcom_gating.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+
+                            if(s_qcom_gating.isChecked())
+                            {
+                                try {
+                                    CommandResult qcom_gating = Shell.SU.run("echo \"1\" > /sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable");
+                                    Toast.makeText(audio_conf.this, "QCOM Gating is Active", Toast.LENGTH_SHORT).show();
+                                }
+                                catch(Exception e){
+                                    e.printStackTrace();
+                                }
+                            }  else {
+                                try {
+                                    CommandResult qcom_gating = Shell.SU.run("echo \"0\" > /sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable");
+                                    Toast.makeText(audio_conf.this, "QCOM Gating is Disabled", Toast.LENGTH_SHORT).show();
+                                }
+                                catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            qcom_gating.setVisibility(View.GONE);
+            Toast.makeText(audio_conf.this, "QCOM Gating Not Found", Toast.LENGTH_LONG).show();
+        }
+
+        if(exp_file.exists()){
+
+        } else {
+            qcom_gating.setVisibility(View.GONE);
         }
 
         Button back = (Button) findViewById(R.id.btn_back);
@@ -395,10 +528,32 @@ public class audio_conf extends AppCompatActivity {
         }
     }
 
+    private void exp_check() {
+        String exp_data = preferences.getString("EXP", "0");
+        if (exp_data.equals("1")) {
+            s_exp.setChecked(true);
+            qcom_gating.setVisibility(View.VISIBLE);
+        } else if (exp_data.equals("0")) {
+            s_exp.setChecked(false);
+            qcom_gating.setVisibility(View.GONE);
+        } else {
+            Toast.makeText(audio_conf.this, "Preference not found", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void qcom_gating_dump() {
+        try {
+            CommandResult qcom_gating_check = Shell.SU.run("cp /sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable /data/data/com.hana.mao/files/qcom_gating.txt");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void Clean() {
         CommandResult IMPEDANCE = Shell.SU.run("rm /data/data/com.hana.mao/files/impedance.txt");
         CommandResult AMP = Shell.SU.run("rm /data/data/com.hana.mao/files/amp.txt");
         CommandResult HPH = Shell.SU.run("rm /data/data/com.hana.mao/files/hph.txt");
         CommandResult UHQA = Shell.SU.run("rm /data/data/com.hana.mao/files/uhqa.txt");
+        CommandResult QCOM_GATING = Shell.SU.run("rm /data/data/com.hana.mao/files/qcom_gating.txt");
     }
 }
